79725d2fa03a9d60b0d1e020e5702126edee45b3
Kevin Zhang <kxwzhang@gmail.com>
2021-02-03 15:47:34 -0800

    Check permissions

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
 
   return (
     <div>
+      <div></div>
       <div id="lp6-gallery" data-widget="gallery" data-contest="1" data-width="100%" data-height="1200" style={{ marginTop: '60px' }} className="lp6-widget-container"></div>
     </div>
   )
