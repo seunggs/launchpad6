66bf099642fae8bbfebb7cac03d80f01f128ca8e
Seungchan Lee <seunggs@gmail.com>
2021-02-03 13:30:00 -0800

    ready for deployment

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <div id="lp6-gallery" dataWidget="gallery" dataContest="1" dataWidth="100%" dataHeight="1200" style={{ marginTop:'70px' }} className="lp6-widget-container"></div>
+      <div id="lp6-gallery" data-widget="gallery" data-contest="1" data-width="100%" data-height="1200" className="lp6-widget-container"></div>
     </div>
   )
 }
