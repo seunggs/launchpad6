a7c44c6e4bcf2a1688efbf0b1249e419dc8e3469
Seungchan Lee <seunggs@gmail.com>
2021-02-03 13:09:34 -0800

    ready for testing

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <div id="lp6-gallery" dataWidget="gallery" dataContest="1" dataWidth="100%" dataHeight="1200" style={{ marginTop:'70px' }} className="lp6-widget-container"></div>
+    </div>
+  )
+}
+
+export default App;
