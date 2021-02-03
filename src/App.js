e2e53f098b82a93f6fb9e2ddf3400625b9a9edaf
Seungchan Lee <seunggs@gmail.com>
2021-02-03 14:23:20 -0800

    add iframe event listner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function App() {
+  useEffect(() => {
+    window.addEventListener('message', event => {
+      // IMPORTANT: check the origin of the data! 
+      if (event.origin.startsWith('https://app.staging.secondstg.com')) {
+        // The data was sent from your site.
+        // Data sent with postMessage is stored in event.data:
+        console.log(event.data);
+      } else {
+        // The data was NOT sent from your site! 
+        // Be careful! Do not use it. This else branch is
+        // here just for clarity, you usually shouldn't need it.
+        return;
+      }
+    })
+  }, [])
+
   return (
     <div>
-      <div id="lp6-gallery" data-widget="gallery" data-contest="1" data-width="100%" data-height="1200" className="lp6-widget-container"></div>
+      <div id="lp6-gallery" data-widget="gallery" data-contest="1" data-width="100%" data-height="1200" style={{ marginTop: '60px' }} className="lp6-widget-container"></div>
     </div>
   )
 }
