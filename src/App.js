c1b0139e7ebe71bf73b82aa5fb58a97dcf857ca6
Kevin Zhang <kxwzhang@gmail.com>
2021-02-03 21:01:57 -0800

    Duplicate a line in App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   return (
     <div className="flex flex-col justify-center text-center">
       <StyledHeadline className="text-center font-bold mt4">AWAKEN</StyledHeadline>
+      <StyledHeadline className="text-center font-bold mt4">AWAKEN</StyledHeadline>
       <div id="lp6-gallery" data-widget="gallery" data-contest="1" data-width="100%" data-height="1200" style={{ marginTop: '60px' }} className="lp6-widget-container"></div>
     </div>
   )
